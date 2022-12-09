a7d8ea3322940c72396f46249c247346e8e98cfb
Nikolay Savin <info@megawebs.kz>
2022-12-10 01:20:33 +0600

    1.0.1

--- _build/includes/functions.php
+++ _build/includes/functions.php
@@ -3,5 +3,5 @@
 function getSnippetContent($filename)
 {
     $o = file_get_contents($filename);
-    return trim(str_replace(array('<?php', '?>'), '', $o));
+    return trim(str_replace(['<?php', '?>'], '', $o));
 }
